b88347d2feb7fef48b9a7a759e0fd4a555c57bb3
Joseph <josephhui@berkeley.edu>
2013-02-28 09:20:58 -0800

    Searching partly implemented
    Changed NumReader to be more general, read only one character at a time
    (reading a line was silly)
    Implemented pal_length (finds palindrome length on two lists)
    Implemented part of overall search
    Some doctests

--- pilindromes.py
+++ pilindromes.py
@@ -1,50 +1,127 @@
-class Pi_Reader(object):
-    """A class to stream out digits of pi.
+from doctest import run_docstring_examples
+
+def rde(fn):
+    run_docstring_examples(fn, None, verbose=True)
+    
+class NumReader(object):
+    """A class to stream out digits of a number stored in a text file.
+    Text file must not contain any numerical digits that are not the number
+    itself (e.g. timestamps). Newlines, spaces, etc. are allowed.
+
+    >>> src = NumReader("pi.txt", 7)
+    >>> src.read(5)
+    [3, 1, 4, 1, 5]
+    >>> src.read(5) #Limit reached
+    [9, 2]
     """
-    def __init__(self):
-        #Buffer to store digits of pi
-        self.digs = []
+    def __init__(self, filename, limit=1000000):
+        #Max number of digits to read out
+        self.limit = limit
+        #Number of digits read so far
+        self.digits_read = 0
         #File to read digits from
-        self.pi_file = open("pi.txt")
-        #Whether or not there are still digits
-        self.has_digits = True
+        self.file = open(filename)
         
-    def get_digits(self, n):
+    def read(self, n):
         """Outputs a list of the next N digits of pi.
         If not enough digits are available, returns as many as possible.
         """
-        #While more digits needed, add more
-        while len(self.digs) < n and self.pi_file.:
-            self.read()
-            
-        #Get required digits
-        n_digs = self.digs[:n]
-        #Remove digits from buffer
-        self.digs = self.digs[n:]
-        #Return digits
-        return n_digs
-
-    def read(self):
-        """Reads a line from the pi file and adds it to the digit buffer.
-        """
-        line = self.pi_file.readline()
+        buffer = [] #Buffer for storing digits to output
         
-        digits = []
-        
-        #Extract all digits from line
-        for char in line:
-            #If digit, add to list of digits
+        #While more digits needed (and limit not reached), add more digits
+        while len(buffer) < n and self.digits_read < self.limit:
+            #Get the next character
+            char = self.file.read(1)
+            #If out of characters, end search
+            if char == '':
+                self.file.close()
+                self.file = None
+                break
+            #Only add numerical digits to the buffer
             if char.isdigit():
-                digits.append(int(char))
-            #Ignore newlines, spaces, etc.
-                
-        #Add digits to buffer
-        self.digs += digits
+                buffer.append(int(char))
+                self.digits_read += 1
+            
+        #Return digits
+        return buffer
 
+    @property
+    def has_digits(self):
+        """Report whether you still have digits left to read or not."""
+        #Reached digit limit
+        if self.digits_read == self.limit:
+            return False
+        #File closed
+        if file is None:
+            return False
+        #Otherwise should be ok
+        return True
 
+def pal_length(past, future):
+    """Given a list PAST and a list FUTURE, determine the length of a
+    palindrome extending outwards into the past/future.
+
+    >>> pal_length([1, 2, 3, 4], [1, 2, 3, 5])
+    3
+    >>> pal_length([1, 2], [1, 2, 3, 5]) #hello => eh, l, lo
+    2
+    >>> pal_length(['e', 'h'], ['l', 'o']) #racecar => car, e, car
+    0
+    >>> pal_length(['c', 'a', 'r'], ['c', 'a', 'r'])
+    3
+    """
+    max_length = min(len(past), len(future))
+    pal_length = 0
+    #Look through all available characters
+    for ch_i in range(max_length):
+        #If palindrome broken, end
+        if past[ch_i] != future[ch_i]:
+            break
+        #If palindrome continues, increment length counter
+        pal_length += 1
+    return pal_length
+        
+"""Plan of attack: Stream digits from a NumReader. Keep past digits, current
+digit, and next digits in lists like this:
+3141592 =>
+Past: 4, 1, 3
+Cur: 1
+Next: 5, 9, 2
+
+Cur might be None. To search for a palindrome, expand outwards on Past and
+Next until you find nonmatching digits; in this case there is no palindrome
+centered at 1, so it would stop immediately.
+512343211
+Past: 3, 2, 1, 5
+Cur: 4
+Next: 3, 2, 1, 1
+This would expand outwards 3 times until finding 5 != 1.
+
+We also search for palindromes with even lengths, e.g.
+51233214
+Past: 3, 2, 1, 5
+Cur: None
+Next: 3, 2, 1, 4
+Expands outwards 3 times.
+"""
 #Source of digits of pi
-source = Pi_Reader()
+source = NumReader("pi.txt")
+#Old digits. Should always be length 100-200, unless there aren't enough digits.
 old_d = []
+#Current digit (possibly None)
 cur_d = source.read(1)[0]
+#Future digits. Should always be length 100-200, unless there aren't enough digits.
 next_d = source.read(100)
-while True:
+
+#Keep running until out of digits
+max_length = 0
+max_pos = None
+while source.has_digits:
+    #Look for palindrome centered at current 
+    cur_length = pal_length(old_d, next_d)
+    if cur_length > max_length:
+        max_length = cur_length
+        max_pos = source.digits_read
+
+    #Look for "even" palindrome centered at current
+    old_d.insert(0, cur_d)
